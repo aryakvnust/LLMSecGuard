acd38c90dfdc7f929988f9dd5b36de0a7bc5cc31
Arya Kavian <146575891+aryakvnust@users.noreply.github.com>
2024-04-26 18:13:04 +0330

    RESET

--- codeanalyzer/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AnalyzeCode
-
-urlpatterns = [
-    path('', AnalyzeCode.as_view(), name='analyze_code')
-]
--- promptdispatcher/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CreatePrompt, ScoreBoard, LLMList, LLMDetails
-
-urlpatterns = [
-    path('createprompt/', CreatePrompt.as_view(), name='createprompt'),
-    path('scoreboard/', ScoreBoard.as_view(), name='scoreboard'),
-    path('models/', LLMList.as_view(), name='models'),
-    path('models/<str:llm_type>', LLMDetails.as_view(), name='models')
-]
